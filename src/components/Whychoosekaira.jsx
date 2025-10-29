d11d06d5d25f70e86d3646d7fed048dc7d75bc8b
Abhimanu Jha <abhimanyujha2015@gmail.com>
2025-10-29 17:23:59 +0530

    ver 2 new branch-1

--- src/components/Whychoosekaira.jsx
+++ src/components/Whychoosekaira.jsx
@@ -56,7 +56,7 @@ export default function WhyChooseKaira() {
 </h1>
 
 
-        {/* Highlights Grid */}
+        {/* Highlights Grid -  */}
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10'>
           {highlights.map((item, index) => (
             <div
